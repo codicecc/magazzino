45ce6ffad2b3cde7195dd149bda85bfb4aae3dea
Francesco Dattolo <info@codice.cc>
2016-12-13 17:45:04 +0100

    Update db.php

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=c23magazzino',
-			'username'   => 'c23euroteck',
-			'password'   => 'nI2M9RIJS46R',
+			'dsn'        => 'mysql:host=localhost;dbname=magazzino',
+			'username'   => 'magazzino',
+			'password'   => 'magazzino',
 		),
 		//'profiling'  => true,
 	),
